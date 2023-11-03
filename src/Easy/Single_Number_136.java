4990169e2fecba130a427afb4de9e46a9d91a47b
RasulMurtuzaliev <ras8705@mail.ru>
2023-11-03 20:02:35 +0500

    third task

---
+++ src/Easy/Single_Number_136.java
@@ -0,0 +1,16 @@
+package Easy;
+
+public class Single_Number_136 {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for (int num : nums) {
+            result ^= num;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Single_Number_136 singleNumber136 = new Single_Number_136();
+        System.out.println(singleNumber136.singleNumber(new int[]{1, 4, 1, 2, 12, 7, 2, 4, 12}));
+    }
+}
