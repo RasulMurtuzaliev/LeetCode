ae1824e922cdbd7ab32dc5220ca5900e94bf3d41
RasulMurtuzaliev <ras8705@mail.ru>
2023-11-05 00:18:39 +0500

    fifth task

---
+++ src/Easy/Valid_Anagram_242.java
@@ -0,0 +1,27 @@
+package Easy;
+
+public class Valid_Anagram_242 {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        int[] count = new int[26];
+        for (char c : s.toCharArray()) {
+            count[c - 'a']++;
+        }
+        for (char c : t.toCharArray()) {
+            count[c - 'a']--;
+        }
+        for (int i : count) {
+            if (i != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Valid_Anagram_242 validAnagram242 = new Valid_Anagram_242();
+        System.out.println(validAnagram242.isAnagram("anagram", "amargan"));
+    }
+}
