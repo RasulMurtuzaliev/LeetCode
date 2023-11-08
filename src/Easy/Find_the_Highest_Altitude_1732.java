d785c7a50a64472aa206f7b42abafdb20a7c6eaa
RasulMurtuzaliev <ras8705@mail.ru>
2023-11-08 22:05:41 +0500

    eighth task

---
+++ src/Easy/Find_the_Highest_Altitude_1732.java
@@ -0,0 +1,22 @@
+package Easy;
+
+public class Find_the_Highest_Altitude_1732 {
+    public int largestAltitude(int[] gain) {
+        int maxAltitude = 0;
+        int sumAltitude = 0;
+
+        for (int g : gain) {
+            sumAltitude += g;
+            maxAltitude = Math.max(maxAltitude, sumAltitude);
+        }
+
+        return maxAltitude;
+    }
+
+    public static void main(String[] args) {
+        Find_the_Highest_Altitude_1732 example = new Find_the_Highest_Altitude_1732();
+        System.out.println(example.largestAltitude(new int[]{1, 2, -3, 4}));
+    }
+}
+//time complexity: O(n)
+//space complexity: O(1)
