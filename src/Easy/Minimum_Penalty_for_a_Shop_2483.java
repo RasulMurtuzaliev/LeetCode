5d0a65f587480ff4b6ed4200e0d5541c7ce887a2
RasulMurtuzaliev <ras8705@mail.ru>
2023-11-09 19:41:16 +0500

    tenth task

---
+++ src/Easy/Minimum_Penalty_for_a_Shop_2483.java
@@ -0,0 +1,25 @@
+package Easy;
+
+public class Minimum_Penalty_for_a_Shop_2483 {
+    public int bestClosingTime(String customers) {
+        int maxPoints = 0;
+        int currentPoints = 0;
+        int bestHour = -1;
+
+        for (int i = 0; i < customers.length(); i++) {
+            currentPoints += customers.charAt(i) == 'Y' ? 1 : -1;
+            if (currentPoints > maxPoints) {
+                maxPoints = currentPoints;
+                bestHour = i;
+            }
+        }
+        return bestHour + 1;
+    }
+
+    public static void main(String[] args) {
+        Minimum_Penalty_for_a_Shop_2483 example = new Minimum_Penalty_for_a_Shop_2483();
+        System.out.println(example.bestClosingTime("YYNYNY"));
+    }
+}
+//time complexity: O(n)
+//space complexity: O(1)
