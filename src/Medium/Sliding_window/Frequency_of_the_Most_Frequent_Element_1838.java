8941cb1d27f7b5a26e758655f375bb422226d9a5
RasulMurtuzaliev <ras8705@mail.ru>
2023-11-24 11:30:19 +0500

    twelfth task

---
+++ src/Medium/Sliding_window/Frequency_of_the_Most_Frequent_Element_1838.java
@@ -0,0 +1,34 @@
+package Medium.Sliding_window;
+
+import java.util.Arrays;
+
+public class Frequency_of_the_Most_Frequent_Element_1838 {
+    public int maxFrequency(int[] nums, int k) {
+        Arrays.sort(nums);
+
+        int left = 0;
+        int sum = 0;
+
+        for (int right = 0; right < nums.length; right++) {
+            int target = nums[right];
+            sum += target;
+
+            int targetSum = target * (right - left + 1);
+
+            if (targetSum - sum > k) {
+                sum -= nums[left];
+                left++;
+            }
+        }
+        return nums.length - left;
+    }
+
+    public static void main(String[] args) {
+        Frequency_of_the_Most_Frequent_Element_1838 example = new Frequency_of_the_Most_Frequent_Element_1838();
+        int[] nums = {1, 2, 4, 5};
+        int k = 5;
+        System.out.println(example.maxFrequency(nums, k));
+    }
+}
+//time complexity: O(nlogn)
+//space complexity: O(1)
